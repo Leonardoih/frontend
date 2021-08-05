ffd5d4d71f84123e2a1ec7549c00bef734c8b300
leonardo <leonardoih090217@gmail.com>
2021-08-04 22:10:26 -0500

    se agrega el proyecto

---
+++ src/app/services/employee.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  constructor() { }
+
+  getEmployees() {
+    return[]
+  }
+}
